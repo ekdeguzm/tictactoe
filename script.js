5f9dd81eba9c4ced1e807876602efb489382c5e2
Kawayan <kawayan.deguzman@gmail.com>
2023-11-18 21:51:07 -0800

    create radio input to determine if player is X or O.

--- script.js
+++ script.js
@@ -1,66 +1,87 @@
-// script.js
 document.addEventListener('DOMContentLoaded', () => {
   const board = document.getElementById('board');
   let currentPlayer = 'X';
+  let gameStarted = false;
 
-  // Create a 3x3 grid
-  for (let i = 0; i < 3; i++) {
+  // Function to create the board
+  function createBoard() {
+    for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-          const cell = document.createElement('div');
-          cell.classList.add('cell');
-          cell.setAttribute('data-row', i);
-          cell.setAttribute('data-col', j);
-          board.appendChild(cell);
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.setAttribute('data-row', i);
+        cell.setAttribute('data-col', j);
+        board.appendChild(cell);
 
-          // Add click event listener to each cell
-          cell.addEventListener('click', handleCellClick);
+        // Add click event listener to each cell
+        cell.addEventListener('click', handleCellClick);
       }
+    }
   }
 
+  // Function to handle cell click
   function handleCellClick(event) {
-      const clickedCell = event.target;
-      const row = clickedCell.getAttribute('data-row');
-      const col = clickedCell.getAttribute('data-col');
+    const clickedCell = event.target;
+    const row = clickedCell.getAttribute('data-row');
+    const col = clickedCell.getAttribute('data-col');
 
-      // Check if the cell is already filled
-      if (!clickedCell.textContent.trim()) {
-          // Update cell with current player's symbol
-          clickedCell.textContent = currentPlayer;
+    // Check if the cell is already filled
+    if (!clickedCell.textContent.trim()) {
+      // Update cell with current player's symbol
+      clickedCell.textContent = currentPlayer;
 
-          // Check for a winner or a tie
-          if (checkWinner(row, col)) {
-              alert(`Player ${currentPlayer} wins!`);
-              resetGame();
-          } else if (checkTie()) {
-              alert("It's a tie!");
-              resetGame();
-          } else {
-              // Switch to the other player
-              currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-          }
+      // Check for a winner or a tie
+      if (checkWinner(row, col)) {
+        alert(`Player ${currentPlayer} wins!`);
+        resetGame();
+      } else if (checkTie()) {
+        alert("It's a tie!");
+        resetGame();
+      } else {
+        // Switch to the other player
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
       }
+    }
   }
 
   // Function to check for a winner
   function checkWinner(row, col) {
-      // Implement your logic to check for a winner
-      // For a simple example, you can check the row, column, and diagonals
-      // Return true if there is a winner, otherwise false
-      return false;
+    // Implement your logic to check for a winner
+    // For a simple example, you can check the row, column, and diagonals
+    // Return true if there is a winner, otherwise false
+    return false;
   }
 
   // Function to check for a tie
   function checkTie() {
-      // Implement your logic to check for a tie
-      // Return true if it's a tie, otherwise false
-      return false;
+    // Implement your logic to check for a tie
+    // Return true if it's a tie, otherwise false
+    return false;
   }
 
   // Function to reset the game
   function resetGame() {
-      // Clear the board and reset any game state variables
-      currentPlayer = 'X';
-      const cells = document.querySelectorAll('.cell');
-      cells.forEach(cell => (cell.textContent = ''));
+    // Clear the board and reset any game state variables
+    currentPlayer = 'X';
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => (cell.textContent = ''));
   }
+
+  // Function to start the game
+  function startGame() {
+    const playerXRadio = document.getElementById('playerX');
+    const playerORadio = document.getElementById('playerO');
+
+    // Set the currentPlayer based on the selected radio button
+    currentPlayer = playerXRadio.checked ? 'X' : 'O';
+
+    if (!gameStarted) {
+      createBoard();
+      gameStarted = true;
+    }
+  }
+
+  // Add click event listener to the "Start Game" button
+  const startButton = document.querySelector('button');
+  startButton.addEventListener('click', startGame);
 });
